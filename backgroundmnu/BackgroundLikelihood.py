ec0e6225cb6e2c04e6c4fa4c9a72338872f3250c
Gabe Lynch <gplynch@ucdavis.edu>
2025-05-14 00:46:40 -0700

    added option to use likelihood with just two parameters

--- backgroundmnu/BackgroundLikelihood.py
+++ backgroundmnu/BackgroundLikelihood.py
@@ -9,7 +9,7 @@ class BackgroundLikelihood(Likelihood):
     input_params = {"omega_b0": None, "omega_cdm0": None, }
     data_path = os.path.join(files("backgroundmnu"), "data")
     data_file = "plikHM_TTTEEE_lowl_lowE"
-
+    mode = "3p"
 
     def initialize(self, **params_values):
         
@@ -22,20 +22,26 @@ class BackgroundLikelihood(Likelihood):
 
         with open(self.data_file_path, "rb") as f:
             loaded = pkl.load(f)
-        
-        self.mean = loaded["mean"]
-        self.cov = loaded["cov"]
+        if self.mode == "3p":
+            self.mean = loaded["mean"]
+            self.cov = loaded["cov"]
+        elif self.mode == "2p":
+            self.mean = loaded["mean"][:2]
+            self.cov = loaded["cov"][:2,:2]
         self.inv_cov = np.linalg.pinv(self.cov)
 
     def get_requirements(self):
-
-        return {"theta_star": None}
+        if self.mode == "3p":   
+            return {"theta_star": None}
+        elif self.mode == "2p":
+            return {}
     
     def logp(self, **params_values):
-        theta_star_100 = 100*self.provider.get_param("theta_star")
         omega_b = params_values['omega_b0']
         omega_c = params_values['omega_cdm0']
-        v = np.array([omega_b, omega_c, theta_star_100])
+        v = np.array([omega_b, omega_c])
+        if self.mode == "3p":
+            v.append(100*self.provider.get_param("theta_star"))
         diff_vector = (v-self.mean)
         chi2 = np.dot(diff_vector, np.matmul(self.inv_cov, diff_vector))
         return -chi2 / 2
